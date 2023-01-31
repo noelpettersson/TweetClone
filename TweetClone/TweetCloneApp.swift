69cf268cf5a75cbf186a6357c3252a25a550f55a
noelpettersson <90799107+noelpettersson@users.noreply.github.com>
2023-01-30 17:51:10 -0800

    Initial Commit

---
+++ TweetClone/TweetCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TweetCloneApp.swift
+//  TweetClone
+//
+//  Created by Noel Pettersson on 2023-01-30.
+//
+
+import SwiftUI
+
+@main
+struct TweetCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
