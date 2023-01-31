6d13d93f88ab8ce840904bc4456c925ad01f6fab
noelpettersson <90799107+noelpettersson@users.noreply.github.com>
2023-01-30 19:51:06 -0800

    Init

--- TweetClone/ContentView.swift
+++ TweetClone/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "person")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Text("AAa!")
+                .padding(.bottom)
+
         }
         .padding()
     }
